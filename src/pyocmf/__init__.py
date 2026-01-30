474b0d1b72f542bf9bc636f7cf07760d027a658c
paul-ww <44506902+paul-ww@users.noreply.github.com>
2026-01-30 08:44:46 +0100

    Read version dynamically

--- src/pyocmf/__init__.py
+++ src/pyocmf/__init__.py
@@ -4,7 +4,9 @@ Parsing, validation, and compliance checking for OCMF metering data from
 electric vehicle charging stations.
 """
 
-__version__ = "0.2.0"
+from importlib.metadata import version
+
+__version__ = version("pyocmf")
 
 from pyocmf.compliance import (
     EichrechtIssue,
